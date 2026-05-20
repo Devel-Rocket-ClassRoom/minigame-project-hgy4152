fc570949de42181b6b4550bc6d87d367ac019ecb
허기영 <hgy4152@gmail.com>
2026-05-20 17:47:14 +0900

    주석추가 및 테스트 SO
    - Warrior 보너스 카드 주석 추가
    - 2-1 보스 SO 추가로 챕터 전환 확인

--- Assets/Scripts/JokerCard/Cards/JokerCard_ClassWarrior.cs
+++ Assets/Scripts/JokerCard/Cards/JokerCard_ClassWarrior.cs
@@ -7,6 +7,7 @@ public class JokerCard_ClassWarrior : JokerCard
     [SerializeField]
     int bonusPerGroup = 2;
 
+    // 판단용으로 현재 구조 사용. classDistribution의 value 값은 항상 1임
     public override int GetBonus(ChainJudge judge) =>
         judge.classDistribution.GetValueOrDefault(ClassType.Warrior) * bonusPerGroup;
 
